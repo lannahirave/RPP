cad641f7999927cae8e498e7f9b3bfcff695ae55
lannahirave <77197415+lannahirave@users.noreply.github.com>
2024-04-25 00:19:47 +0300

    mpi fixed

--- mpi.py
+++ mpi.py
@@ -1,8 +1,6 @@
-import time
 
-import numpy as np
 from mpi4py import MPI
-
+from time import time
 from generate_matrice_to_file import read_matrix_from_file
 
 
@@ -13,54 +11,53 @@ def save_matrix_to_file(matrix, filename):
 
 
 def multiply_matrices(matrix1, matrix2):
-    return np.dot(matrix1, matrix2)
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix2[0])):
+            row.append(sum(matrix1[i][k] * matrix2[k][j] for k in range(len(matrix1[0]))))
+        result.append(row)
+    return result
 
 
 def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-    
+
+    matrix2 = None
+    rowsNum = 0
+    chunks = []
+
     if rank == 0:
         matrix = read_matrix_from_file("matrix.txt")
-        n = len(matrix)
-        matrix1 = np.matrix(matrix)
-        matrix2 = np.matrix(matrix)
+        time_start = time()
+        matrix1 = matrix.copy()
+        rowsNum = len(matrix1)
+        matrix2 = matrix.copy()
 
-        start_time = time.time()
-
-        # розкидуєм по процесах
-        comm.bcast(n, root=0)
-        local_matrix1 = np.empty((n // size, n), dtype=int)
-        comm.Scatter(matrix1, local_matrix1, root=0)
-
-        matrix2 = comm.bcast(matrix2, root=0)
-
-        local_result = multiply_matrices(local_matrix1, matrix2)
-
-
-        global_result = None
+        chunkSize = rowsNum // size
+        for i in range(size-1):
+            chunk = []
+            for ii in range(chunkSize):
+                chunk.append(matrix1[ii + i * chunkSize])
+            chunks.append(chunk)
+        chunks.append(matrix1[chunkSize * (size-1):])
         
-        global_result = np.empty((n, n), dtype=int)
-        comm.Gather(local_result, global_result, root=0)
 
-        end_time = time.time()
-        if rank == 0:
-            print("Time mpi:", end_time - start_time, "seconds")
-            save_matrix_to_file(global_result, "mpi_matrix.txt")
+    matrix2 = comm.bcast(matrix2, root=0)
+    rowsNum = comm.bcast(rowsNum, root=0)
+    chunk = comm.scatter(chunks, root=0)
 
-    else:
-        n = comm.bcast(None, root=0)
+    chunkResult = multiply_matrices(chunk, matrix2)
 
-        local_matrix1 = np.empty((n // size, n), dtype=int)
+    result = comm.gather(chunkResult, root=0)
 
-        comm.Scatter(None, local_matrix1, root=0)
-
-        matrix2 = comm.bcast(None, root=0)
-
-        local_result = multiply_matrices(local_matrix1, matrix2)
-        comm.Gather(local_result, None, root=0)
-
+    if rank == 0:
+        # flatten the result
+        result = [item for sublist in result for item in sublist]
+        time_end = time()
+        print(f"Time: {time_end - time_start} seconds")
 
 if __name__ == "__main__":
     main()
